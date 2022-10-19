3b4d8e4ca0a8766a6c1bc139285bb7c1a4fa35d9
Stefano Terragni <stefano.terragni@everis.com>
2022-10-19 19:08:19 +0200

    Initial Commit

---
+++ src/main/java/it/gov/pagopa/initiative/statistics/InitiativeStatisticsApplication.java
@@ -0,0 +1,15 @@
+package it.gov.pagopa.initiative.statistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class InitiativeStatisticsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InitiativeStatisticsApplication.class, args);
+  }
+
+}
+
