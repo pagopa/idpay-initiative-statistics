2223ede4cc34f749e63fe5fb641e2b7b24b5feb0
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2023-01-10 15:41:23 +0100

    Enabling web exposure of info actuator with git information (#21)
    * Enabling web exposure of info actuator with git information
    
    * fixing unit test
    
    * relaxing unit test

--- src/main/java/it/gov/pagopa/initiative/statistics/service/BaseStatisticsEvaluationService.java
+++ src/main/java/it/gov/pagopa/initiative/statistics/service/BaseStatisticsEvaluationService.java
@@ -76,6 +76,7 @@ public abstract class BaseStatisticsEvaluationService<E, I> implements Statistic
 
         AtomicLong maxOffsetAtomic = new AtomicLong(-1);
 
+        //noinspection SimplifyStreamApiCallChains: the peek method is a bad choice, suppressing substitution suggestion
         Map<String, List<Triple<ConsumerRecord<String, String>, String, I>>> groupByInitiative = records.parallelStream()
                 // deserializing and returning pair of record and entity
                 .map(r -> {
@@ -91,7 +92,10 @@ public abstract class BaseStatisticsEvaluationService<E, I> implements Statistic
                 // skipping deserialization failed records
                 .filter(Objects::nonNull)
                 // storing maxOffsetAtomic of valid records
-                .peek(r2e -> maxOffsetAtomic.getAndUpdate(o -> Math.max(o, r2e.getKey().offset())))
+                .map(r2e -> {
+                    maxOffsetAtomic.getAndUpdate(o -> Math.max(o, r2e.getKey().offset()));
+                    return r2e;
+                })
                 // skipping retry messages scheduled by other application
                 .filter(this::isNotRetry)
                 // transforming the record2entity stream into a pair record2initiativeBased stream
