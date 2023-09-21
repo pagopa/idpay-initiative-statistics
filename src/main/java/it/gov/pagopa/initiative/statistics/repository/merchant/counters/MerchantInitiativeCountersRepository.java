37040e9e3e7e5b0c03f444b6a675a16a9e566a3d
SalvatorManna <salvatore11.manna@emea.nttdata.com>
2023-09-21 15:09:12 +0200

    IDP-1852-Modified CommandOperationDTO, added paged delete method to MerchantCountersOpsRepository, modified JUnit tests.

--- src/main/java/it/gov/pagopa/initiative/statistics/repository/merchant/counters/MerchantInitiativeCountersRepository.java
+++ src/main/java/it/gov/pagopa/initiative/statistics/repository/merchant/counters/MerchantInitiativeCountersRepository.java
@@ -3,8 +3,5 @@ package it.gov.pagopa.initiative.statistics.repository.merchant.counters;
 import it.gov.pagopa.initiative.statistics.model.MerchantInitiativeCounters;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
 public interface MerchantInitiativeCountersRepository extends MongoRepository<MerchantInitiativeCounters, String>, MerchantInitiativeCountersOpsRepository {
-    List<MerchantInitiativeCounters> deleteByInitiativeId(String initiativeId);
 }
--- src/main/java/it/gov/pagopa/initiative/statistics/service/commands/ops/DeleteInitiativeServiceImpl.java
+++ src/main/java/it/gov/pagopa/initiative/statistics/service/commands/ops/DeleteInitiativeServiceImpl.java
@@ -1,17 +1,25 @@
 package it.gov.pagopa.initiative.statistics.service.commands.ops;
 
+import it.gov.pagopa.initiative.statistics.dto.events.CommandOperationDTO;
+import it.gov.pagopa.initiative.statistics.model.MerchantInitiativeCounters;
 import it.gov.pagopa.initiative.statistics.repository.InitiativeStatRepository;
 import it.gov.pagopa.initiative.statistics.repository.merchant.counters.MerchantInitiativeCountersRepository;
 import it.gov.pagopa.initiative.statistics.utils.AuditUtilities;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @Slf4j
+@SuppressWarnings("BusyWait")
 public class DeleteInitiativeServiceImpl implements DeleteInitiativeService{
     private final InitiativeStatRepository initiativeStatRepository;
     private final MerchantInitiativeCountersRepository merchantInitiativeCountersRepository;
     private final AuditUtilities auditUtilities;
+    private static final String PAGINATION_KEY = "pagination";
+    private static final String DELAY_KEY = "delay";
 
     public DeleteInitiativeServiceImpl(InitiativeStatRepository initiativeStatRepository, MerchantInitiativeCountersRepository merchantInitiativeCountersRepository, AuditUtilities auditUtilities) {
         this.initiativeStatRepository = initiativeStatRepository;
@@ -20,13 +28,27 @@ public class DeleteInitiativeServiceImpl implements DeleteInitiativeService{
     }
 
     @Override
-    public void execute(String initiativeId) {
-        initiativeStatRepository.deleteById(initiativeId);
-        log.info("[DELETE_INITIATIVE] Deleted initiative {} from collection: initiative_statistics", initiativeId);
-        auditUtilities.logDeletedInitiativeStatistics(initiativeId);
-        merchantInitiativeCountersRepository.deleteByInitiativeId(initiativeId)
-                .forEach(merchantCounter ->
-                    auditUtilities.logDeletedMerchantCounter(merchantCounter.getMerchantId(), initiativeId));
-        log.info("[DELETE_INITIATIVE] Deleted initiative {} from collection: merchant_initiative_counters", initiativeId);
+    public void execute(CommandOperationDTO payload) {
+        initiativeStatRepository.deleteById(payload.getEntityId());
+        log.info("[DELETE_INITIATIVE] Deleted initiative {} from collection: initiative_statistics", payload.getEntityId());
+        auditUtilities.logDeletedInitiativeStatistics(payload.getEntityId());
+
+        List<MerchantInitiativeCounters> deletedMerchantInitiativeCounters = new ArrayList<>();
+        List<MerchantInitiativeCounters> fetchedMerchantInitiativeCounters;
+
+        do {
+            fetchedMerchantInitiativeCounters = merchantInitiativeCountersRepository.deletePaged(payload.getEntityId(),
+                    Integer.parseInt(payload.getAdditionalParams().get(PAGINATION_KEY)));
+            deletedMerchantInitiativeCounters.addAll(fetchedMerchantInitiativeCounters);
+            try{
+                Thread.sleep(Long.parseLong(payload.getAdditionalParams().get(DELAY_KEY)));
+            } catch (InterruptedException e){
+                log.error("An error has occurred while waiting {}", e.getMessage());
+                Thread.currentThread().interrupt();
+            }
+        } while (fetchedMerchantInitiativeCounters.size() == (Integer.parseInt(payload.getAdditionalParams().get(PAGINATION_KEY))));
+
+        deletedMerchantInitiativeCounters.forEach(merchantCounter -> auditUtilities.logDeletedMerchantCounter(merchantCounter.getMerchantId(), payload.getEntityId()));
+        log.info("[DELETE_INITIATIVE] Deleted initiative {} from collection: merchant_initiative_counters", payload.getEntityId());
     }
 }
