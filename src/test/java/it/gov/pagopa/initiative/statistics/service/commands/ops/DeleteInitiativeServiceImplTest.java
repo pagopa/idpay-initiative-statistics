c0e03437e9486db3b5a4e272787b49b6082011da
SalvatorManna <117901709+SalvatorManna@users.noreply.github.com>
2023-09-27 17:47:48 +0200

    IDP-1852-Added pagination and delay to application.yml and values environments. Modified JUnit tests (#89)

--- src/test/java/it/gov/pagopa/initiative/statistics/service/commands/ops/DeleteInitiativeServiceImplTest.java
+++ src/test/java/it/gov/pagopa/initiative/statistics/service/commands/ops/DeleteInitiativeServiceImplTest.java
@@ -5,62 +5,62 @@ import it.gov.pagopa.initiative.statistics.model.MerchantInitiativeCounters;
 import it.gov.pagopa.initiative.statistics.repository.InitiativeStatRepository;
 import it.gov.pagopa.initiative.statistics.repository.merchant.counters.MerchantInitiativeCountersRepository;
 import it.gov.pagopa.initiative.statistics.utils.AuditUtilities;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith({MockitoExtension.class, SpringExtension.class})
+@ContextConfiguration(classes = DeleteInitiativeServiceImpl.class)
+@TestPropertySource(
+        locations = "classpath:application.yml",
+        properties = {
+                "app.delete.paginationSize=100",
+                "app.delete.delayTime=1000"
+        })
 class DeleteInitiativeServiceImplTest {
+    @Autowired
     private DeleteInitiativeService deleteInitiativeService;
-    @Mock
+    @MockBean
     private InitiativeStatRepository initiativeStatRepository;
-    @Mock
+    @MockBean
     private MerchantInitiativeCountersRepository merchantInitiativeCountersRepository;
-    @Mock
+    @MockBean
     private AuditUtilities auditUtilities;
-    private static final String PAGINATION_KEY = "pagination";
-    private static final String DELAY_KEY = "delay";
-    private static final String PAGINATION_VALUE = "100";
-    private static final String DELAY_VALUE = "1500";
     private static final String INITIATIVE_ID = "INITIATIVE_ID";
     private static final String MERCHANT_INITIATIVE_COUNTERS_ID = "INITIATIVE_COUNTER_IS";
     private static final String OPERATION_TYPE_DELETE_INITIATIVE = "DELETE_INITIATIVE";
 
-    @BeforeEach
-    void setUp() {
-        deleteInitiativeService = new DeleteInitiativeServiceImpl(initiativeStatRepository, merchantInitiativeCountersRepository, auditUtilities);
-    }
+    @Value("${app.delete.paginationSize}")
+    String pagination;
 
     @Test
     void processCommand_deleteTransactions() {
         // Given
-        Map<String, String> additionalParams = new HashMap<>();
-        additionalParams.put(PAGINATION_KEY, PAGINATION_VALUE);
-        additionalParams.put(DELAY_KEY, DELAY_VALUE);
         final CommandOperationDTO queueCommandOperationDTO = CommandOperationDTO.builder()
                 .entityId(INITIATIVE_ID)
                 .operationType(OPERATION_TYPE_DELETE_INITIATIVE)
                 .operationTime(LocalDateTime.now().minusMinutes(5))
-                .additionalParams(additionalParams)
                 .build();
         MerchantInitiativeCounters merchantInitiativeCounters = new MerchantInitiativeCounters();
         merchantInitiativeCounters.setId(MERCHANT_INITIATIVE_COUNTERS_ID);
         merchantInitiativeCounters.setInitiativeId(INITIATIVE_ID);
         final List<MerchantInitiativeCounters> deletedPage = List.of(merchantInitiativeCounters);
 
-        final List<MerchantInitiativeCounters> merchantInitiativeCountersPage = createMerchantInitiativeCountersPage(Integer.parseInt(PAGINATION_VALUE));
-        when(merchantInitiativeCountersRepository.deletePaged(queueCommandOperationDTO.getEntityId(), Integer.parseInt(queueCommandOperationDTO.getAdditionalParams().get(PAGINATION_KEY))))
+        final List<MerchantInitiativeCounters> merchantInitiativeCountersPage = createMerchantInitiativeCountersPage(Integer.parseInt(pagination));
+        when(merchantInitiativeCountersRepository.deletePaged(queueCommandOperationDTO.getEntityId(), Integer.parseInt(pagination)))
                 .thenReturn(merchantInitiativeCountersPage)
                 .thenReturn(deletedPage);
         Thread.currentThread().interrupt();
@@ -69,7 +69,7 @@ class DeleteInitiativeServiceImplTest {
         deleteInitiativeService.execute(queueCommandOperationDTO);
 
         // Then
-        Mockito.verify(merchantInitiativeCountersRepository, Mockito.times(2)).deletePaged(queueCommandOperationDTO.getEntityId(), Integer.parseInt(queueCommandOperationDTO.getAdditionalParams().get(PAGINATION_KEY)));
+        Mockito.verify(merchantInitiativeCountersRepository, Mockito.times(2)).deletePaged(queueCommandOperationDTO.getEntityId(), Integer.parseInt(pagination));
     }
 
     private List<MerchantInitiativeCounters> createMerchantInitiativeCountersPage(int pageSize){
