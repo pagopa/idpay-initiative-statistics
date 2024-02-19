0f298852211386b0d4594040c14381850e25ca63
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2024-02-19 15:29:53 +0100

    [IPD-2367] Change incoming transaction filtering logic

--- src/main/java/it/gov/pagopa/initiative/statistics/utils/Constants.java
+++ src/main/java/it/gov/pagopa/initiative/statistics/utils/Constants.java
@@ -1,14 +1,12 @@
 package it.gov.pagopa.initiative.statistics.utils;
 
-import java.util.List;
 
 public final class Constants {
     private Constants(){}
 
     //region trx constants
     public static final String TRX_STATUS_AUTHORIZED = "AUTHORIZED";
+    public static final String TRX_STATUS_REWARDED = "REWARDED";
     public static final String TRX_STATUS_CANCELLED = "CANCELLED";
-
-    public static final List<String> EXCLUDED_TRX_STATUSES = List.of(TRX_STATUS_AUTHORIZED, TRX_STATUS_CANCELLED);
     //endregion
 }
