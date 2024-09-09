d7c370c814532014d10a9514e385889e78b2c230
stedelia <144045955+stedelia@users.noreply.github.com>
2024-09-09 16:21:11 +0200

    chore: fix CVE (#136)
    * chore: update spring boot parent and docker
    
    * chore: update spring boot parent and docker

--- src/test/java/it/gov/pagopa/common/kafka/KafkaTestUtilitiesService.java
+++ src/test/java/it/gov/pagopa/common/kafka/KafkaTestUtilitiesService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.gov.pagopa.common.kafka.utils.KafkaConstants;
 import it.gov.pagopa.common.utils.TestUtils;
+import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.OffsetSpec;
 import org.apache.kafka.clients.admin.RecordsToDelete;
 import org.apache.kafka.clients.admin.TopicListing;
@@ -26,7 +27,6 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
-import org.springframework.kafka.test.EmbeddedKafkaKraftBroker;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
@@ -53,7 +53,7 @@ public class KafkaTestUtilitiesService {
 
     public static final String GROUPID_TEST_CHECK = "idpay-group-test-check";
     @Autowired
-    private EmbeddedKafkaKraftBroker kafkaBroker;
+    private EmbeddedKafkaBroker kafkaBroker;
     @Autowired
     private KafkaTemplate<byte[], byte[]> template;
 
@@ -79,28 +79,30 @@ public class KafkaTestUtilitiesService {
 
     @AfterTestClass
     void clearTopics() {
-        kafkaBroker.doWithAdmin(admin -> {
-            try {
-                Collection<TopicListing> topics = admin.listTopics().listings().get();
-                admin.deleteRecords(
-                                admin.listOffsets(
-                                                topics.stream()
-                                                        .filter(topicListing -> !topicListing.isInternal())
-                                                        .flatMap(t -> IntStream.range(0, kafkaBroker.getPartitionsPerTopic())
-                                                                .boxed()
-                                                                .map(p -> new TopicPartition(t.name(), p)))
-                                                        .collect(Collectors.toMap(tp -> tp,
-                                                                tp -> OffsetSpec.latest()))
-                                        ).all().get().entrySet().stream()
-                                        .filter(e -> e.getValue().offset() > 0)
-                                        .collect(Collectors.toMap(Map.Entry::getKey,
-                                                e -> RecordsToDelete.beforeOffset(e.getValue().offset()))))
-                        .all().get();
+        Map<String, Object> brokerConfig = Map.of(
+                "bootstrap.servers", kafkaBroker.getBrokersAsString()
+        );
 
-            } catch (InterruptedException | ExecutionException e) {
-                throw new IllegalStateException("Something gone wrong while emptying topics", e);
-            }
-        });
+        try (AdminClient admin = AdminClient.create(brokerConfig)) {
+            Collection<TopicListing> topics = admin.listTopics().listings().get();
+            admin.deleteRecords(
+                            admin.listOffsets(
+                                            topics.stream()
+                                                    .filter(topicListing -> !topicListing.isInternal())
+                                                    .flatMap(t -> IntStream.range(0, kafkaBroker.getPartitionsPerTopic())
+                                                            .boxed()
+                                                            .map(p -> new TopicPartition(t.name(), p)))
+                                                    .collect(Collectors.toMap(tp -> tp,
+                                                            tp -> OffsetSpec.latest()))
+                                    ).all().get().entrySet().stream()
+                                    .filter(e -> e.getValue().offset() > 0)
+                                    .collect(Collectors.toMap(Map.Entry::getKey,
+                                            e -> RecordsToDelete.beforeOffset(e.getValue().offset()))))
+                    .all().get();
+
+        } catch (InterruptedException | ExecutionException e) {
+            throw new IllegalStateException("Something went wrong while clearing topics", e);
+        }
     }
 
     /** It will return usefull URLs related to embedded kafka */
@@ -219,10 +221,10 @@ public class KafkaTestUtilitiesService {
             headers = Stream.concat(
                             Arrays.stream(additionalHeaders),
                             StreamSupport.stream(headers.spliterator(), false))
-                    .collect(Collectors.toList());
+                    .toList();
         }
-        ProducerRecord<byte[], byte[]> record = new ProducerRecord<>(topic, partition, key == null ? null : key.getBytes(StandardCharsets.UTF_8), payload.getBytes(StandardCharsets.UTF_8), headers);
-        template.send(record);
+        ProducerRecord<byte[], byte[]> rec = new ProducerRecord<>(topic, partition, key == null ? null : key.getBytes(StandardCharsets.UTF_8), payload.getBytes(StandardCharsets.UTF_8), headers);
+        template.send(rec);
     }
 //endregion
 
@@ -278,13 +280,13 @@ public class KafkaTestUtilitiesService {
     //region error topic
     public void checkErrorsPublished(String topicErrors, Pattern errorUseCaseIdPatternMatch, int expectedErrorMessagesNumber, long maxWaitingMs, List<Pair<Supplier<String>, java.util.function.Consumer<ConsumerRecord<String, String>>>> errorUseCases) {
         final List<ConsumerRecord<String, String>> errors = consumeMessages(topicErrors, expectedErrorMessagesNumber, maxWaitingMs);
-        for (final ConsumerRecord<String, String> record : errors) {
-            final Matcher matcher = errorUseCaseIdPatternMatch.matcher(record.value());
+        for (final ConsumerRecord<String, String> rec : errors) {
+            final Matcher matcher = errorUseCaseIdPatternMatch.matcher(rec.value());
             int useCaseId = matcher.find() ? Integer.parseInt(matcher.group(1)) : -1;
             if (useCaseId == -1) {
-                throw new IllegalStateException("UseCaseId not recognized! " + record.value());
+                throw new IllegalStateException("UseCaseId not recognized! " + rec.value());
             }
-            errorUseCases.get(useCaseId).getSecond().accept(record);
+            errorUseCases.get(useCaseId).getSecond().accept(rec);
         }
     }
 
